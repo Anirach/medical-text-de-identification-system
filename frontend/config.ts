499ba44c66564bd80e95dce94aa172c3bddfd3be
Leap <git@leap.new>
2025-10-22 00:38:39 +0000

    Implement requested change

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  clerkPublishableKey: "pk_test_YXdha2UtaGVycmluZy00Mi5jbGVyay5hY2NvdW50cy5kZXYk",
+  clerkPublishableKey: "pk_test_cHJvdmVuLWNpdmV0LTkwLmNsZXJrLmFjY291bnRzLmRldiQ",
 };
